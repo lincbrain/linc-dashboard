18ad19eb3a1457771a8623d37d03025620556ea7
Kabilar Gunalan <kabi@mit.edu>
2025-04-24 23:28:23 -0500

    Update homepage with summary across datasets and modalities

--- pages/home.py
+++ pages/home.py
@@ -1,54 +1,59 @@
-from pathlib import Path
-from dandi.dandiapi import DandiAPIClient
 import pandas as pd
 import streamlit as st
-import json
+import numpy as np
+import plotly.express as px
 
 @st.cache_data(ttl='1d')
-def index_files():
-    client = DandiAPIClient("https://api.lincbrain.org/api")
-    client.dandi_authenticate()
-
-    df_dandisets = pd.DataFrame(columns=["Dandiset"])
-
-    for dandiset in client.get_dandisets():
-        latest_dandiset = dandiset.for_version('draft')
-        df_dandisets.loc[len(df_dandisets)] = [dandiset.identifier]
-        print(df_dandisets)
-        j=0
-
-        for asset in latest_dandiset.get_assets():
-            metadata = asset.get_metadata()
-            metadata_dict = metadata.json_dict()
-            print(json.dumps(metadata_dict, indent=4))
-            asset_path = metadata_dict.get('path')
-
-            if metadata_dict.get('wasAttributedTo') is not None and len(metadata_dict.get('wasAttributedTo')) > 0:
-                subject = metadata_dict.get('wasAttributedTo')[0]['identifier']
-            else:
-                subject = ''
-
-            # df_assets.loc[len(df_assets)] = [dandiset.identifier, subject, ''.join(Path(asset_path).suffixes), Path(asset_path).name, asset_path]
-                
-            j=j+1
-            if j==2:
-                break
-
-    return df_dandisets
-
 def main():
-    st.title("LINC Data Summary Site")
-    st.write("This is meant to share information across the LINC project investigators.  Data is indexed daily.  If your data is not listed here it is likely that it does not adhere to the BIDS specification and the file names will need to be changed.")
+    st.title("LINC Dashboard")
+    st.write("Displayed is a summary of all data on lincbrain.org.  This dashboard is intended for the LINC project investigators.  Data is indexed daily.")
 
-    df_dandisets = index_files()
-    print(df_dandisets)
-    print(df_dandisets['Dandiset'].unique())
-    tabs = st.tabs(["All Dandisets"] + list(df_dandisets['Dandiset'].unique()))
+    # List summary of all datasets
+    st.title("Datasets")
 
-    with tabs[0]:
-        st.title("Summary of data modalities acquired")
-        st.dataframe(df_dandisets)
-    with tabs[1]:
-        st.header("Test1")
-    with tabs[2]:
-        st.header("Test2")
+    df_datasets = pd.read_csv('lincbrain_datasets.csv')
+    df_datasets_sorted = df_datasets.sort_values(by='Dataset')
+
+    st.dataframe(df_datasets_sorted, hide_index=True, column_config={col: {'alignment':"left"} for col in df_datasets_sorted.columns})
+
+    # Plot summary of all modalities
+    st.title("Modalities")
+    df_modalities = pd.read_csv('lincbrain_modalities.csv')
+    df_modalities_sorted = df_modalities.sort_values(by='Modality')
+    
+    subjects_unique = list(set([value.strip() for item in df_modalities_sorted['Subject'] 
+                                if isinstance(item, str) 
+                                for value in item.split(',')]))
+
+    grid_values = np.zeros((df_modalities_sorted['Modality'].nunique(),
+                            len(subjects_unique)))
+
+    for m, modality in enumerate(df_modalities_sorted['Modality'].unique()):
+        for s, subject in enumerate(subjects_unique):
+            
+            subject_modality = list(set([value.strip() 
+                                          for item in list(df_modalities_sorted[df_modalities_sorted['Modality'] == modality]['Subject']) 
+                                if isinstance(item, str) 
+                                for value in item.split(',')]))
+
+            if subject in subject_modality:
+                grid_values[m,s] = 1
+
+    fig = px.imshow(grid_values, 
+                    x=subjects_unique, 
+                    y=df_modalities_sorted['Modality'].unique(), 
+                    color_continuous_scale=['rgba(102,102,102,1)',
+                                            'rgba(99,110,250,1)'])
+    
+    fig.update_layout(plot_bgcolor='rgba(0,0,0,0)', 
+                      coloraxis_showscale=False, 
+                      margin=dict(l=10, r=10, t=10, b=10),
+                      xaxis_type='category')
+
+    fig.update_traces(xgap=5, 
+                      ygap=5)
+
+    st.plotly_chart(fig, use_container_width=True)
+
+    # List summary of all modalities
+    st.dataframe(df_modalities_sorted, hide_index=True, column_config={col: {'alignment':"left"} for col in df_modalities_sorted.columns})
