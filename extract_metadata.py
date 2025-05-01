43eaad231f86bf5460d06e170498aedcc2c71204
Kabilar Gunalan <kabi@mit.edu>
2025-05-01 11:19:43 -0500

    Ignore datasets that were not acquired as part of LINC milestones and rename variable

--- extract_metadata.py
+++ extract_metadata.py
@@ -27,40 +27,41 @@ def extract_assets():
                             "Directory", # Top-level directory (e.g. source data, raw data, derivatives)
                             'Size (bytes)'])
 
-    for dandiset in dandisets:
-        latest_dandiset = dandiset.for_version('draft')
-        for asset in latest_dandiset.get_assets():
-            asset_split = asset.path.split('/')
+    for dataset in dandisets:
+        latest_dataset = dataset.for_version('draft')
+        if latest_dataset.identifier not in ['000048', '000004']: # Exclude OpenBNB dataset and mouse LSM dataset
+            for asset in latest_dataset.get_assets():
+                asset_split = asset.path.split('/')
 
-            print(f"Dandiset: {latest_dandiset}; Asset: {asset_split[-1]:<40}", end='\r')
+                print(f"Dataset: {latest_dataset}; Asset: {asset_split[-1]:<80}", end='\r')
 
-            metadata = asset.get_metadata()
-            metadata_dict = metadata.model_dump(mode='json', exclude_none=True)
+                metadata = asset.get_metadata()
+                metadata_dict = metadata.model_dump(mode='json', exclude_none=True)
 
-            subject = 'Unknown'
-            for part in asset_split:
-                if part.startswith("sub-"):
-                    subject = part.split("sub-")[1].split('_')[0]
-                    break
+                subject = 'Unknown'
+                for part in asset_split:
+                    if part.startswith("sub-"):
+                        subject = part.split("sub-")[1].split('_')[0]
+                        break
 
-            if subject == 'Unknown' and any(filename in asset_split[-1].lower() for filename in ['dataset_description.json', 'participants.tsv', 'readme.md', 'samples.tsv']):
-                subject = 'n/a'
+                if subject == 'Unknown' and any(filename in asset_split[-1].lower() for filename in ['dataset_description.json', 'participants.tsv', 'readme.md', 'samples.tsv']):
+                    subject = 'n/a'
 
-            modality = next((value for key, value in modalities.items() 
-                            if key in asset_split[-1].lower()), 
-                            'Unknown')
+                modality = next((value for key, value in modalities.items() 
+                                if key in asset_split[-1].lower()), 
+                                'Unknown')
 
-            suffix = Path(asset.path).suffixes[0][1:] if Path(asset.path).suffixes else ''
+                suffix = Path(asset.path).suffixes[0][1:] if Path(asset.path).suffixes else ''
 
-            df.loc[len(df)] = [latest_dandiset.identifier,
-                                latest_dandiset.version.identifier,
-                                subject,
-                                modality,
-                                asset.path, 
-                                asset_split[-1],
-                                suffix,
-                                asset_split[0],
-                                metadata_dict['contentSize']]
+                df.loc[len(df)] = [latest_dataset.identifier,
+                                    latest_dataset.version.identifier,
+                                    subject,
+                                    modality,
+                                    asset.path, 
+                                    asset_split[-1],
+                                    suffix,
+                                    asset_split[0],
+                                    metadata_dict['contentSize']]
 
     return df
 
