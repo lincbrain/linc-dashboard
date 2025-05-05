729c6e65811e88fb2e95eed8526cdd443b4ea156
Kabilar Gunalan <kabi@mit.edu>
2025-05-05 12:10:24 -0500

    Add utility function to load CSV file from S3

--- pages/files.py
+++ pages/files.py
@@ -1,19 +1,11 @@
 import os
 import pandas as pd
 import streamlit as st
+from ..utils import load_dataset
 
 def main():
     st.title("Browse and search all files on lincbrain.org")
 
-    AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-    AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-
-    df = pd.read_csv(
-        f"s3://linc-dashboard/lincbrain_assets.csv",
-        storage_options={
-            "key": AWS_ACCESS_KEY_ID,
-            "secret": AWS_SECRET_ACCESS_KEY,
-        },
-    )
+    df = load_dataset("lincbrain_assets.csv")
 
     st.dataframe(df)
