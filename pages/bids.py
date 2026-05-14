3502ac1e9197d5c4511d5aaf5bcef2c4259cadb7
Kabilar Gunalan <kabi@mit.edu>
2026-05-14 16:44:46 -0500

    Update scripts to read csv files from repository

--- pages/bids.py
+++ pages/bids.py
@@ -1,9 +1,9 @@
+import pandas as pd
 import streamlit as st
-from utils import load_dataset
 
 def main():
     st.title("All non-BIDS compliant files on lincbrain.org")
 
-    df = load_dataset("lincbrain_assets.csv")
+    df = pd.read_csv("lincbrain_assets.csv")
 
     st.dataframe(df[df['Subject']=='Unknown'])
--- pages/files.py
+++ pages/files.py
@@ -1,9 +1,9 @@
+import pandas as pd
 import streamlit as st
-from utils import load_dataset
 
 def main():
     st.title("Browse and search all files on lincbrain.org")
 
-    df = load_dataset("lincbrain_assets.csv")
+    df = pd.read_csv("lincbrain_assets.csv")
 
     st.dataframe(df)
